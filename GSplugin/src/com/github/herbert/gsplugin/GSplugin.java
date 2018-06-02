220fd0cdef706fb3726157ba3fe57be4573fa6c9
Herbert <herbertsfundgrube@emailn.de>
2018-06-02 06:04:45 +0200

    Hauptklasse initialisiert

--- GSplugin/src/com/github/herbert/gsplugin/GSplugin.java
+++ GSplugin/src/com/github/herbert/gsplugin/GSplugin.java
@@ -1,5 +1,13 @@
 package com.github.herbert.gsplugin;
 
-public class GSplugin {
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class GSplugin extends JavaPlugin {
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
 }
