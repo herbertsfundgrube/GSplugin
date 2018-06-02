958bd7bdd9e06c52e370be74b75553c7f7e81bd4
Herbert <herbertsfundgrube@emailn.de>
2018-06-02 21:20:55 +0200

    CommandExecutor auf den Befehl /gs reduziert

--- GSplugin/src/com/github/herbert/gsplugin/GSplugin.java
+++ GSplugin/src/com/github/herbert/gsplugin/GSplugin.java
@@ -2,12 +2,18 @@ package com.github.herbert.gsplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.github.herbert.gsplugin.ce.GSCommandExecutor;
+
 public class GSplugin extends JavaPlugin {
+	
+	private GSCommandExecutor ce;
+	
 	public void onEnable() {
-		
+		ce = new GSCommandExecutor(this);
+		getCommand("gs").setExecutor(ce); //CommandExecutor für Befehl "/gs" setzen
 	}
 	
 	public void onDisable() {
-		
+		this.getLogger().info("GSplugin deaktiviert");
 	}
 }
