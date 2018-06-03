aa0916911cca882d1560f37107a1a8ac7bd67e20
Herbert <herbertsfundgrube@emailn.de>
2018-06-03 13:45:44 +0200

    fix

--- GSplugin/src/com/github/herbert/gsplugin/ce/GSCommandExecutor.java
+++ GSplugin/src/com/github/herbert/gsplugin/ce/GSCommandExecutor.java
@@ -18,7 +18,7 @@ public class GSCommandExecutor implements org.bukkit.command.CommandExecutor {
 	//onCommand - Ausgabe "false", wenn ein Fehler beim Ausführen des Befehls aufgetreten ist.
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if(args==null)
+		if(args[0]==null)
 			return false;
 		if(args[0].equalsIgnoreCase("test")) {
 			sender.sendMessage("Du hast den Befehl /gs test eingegeben.");
