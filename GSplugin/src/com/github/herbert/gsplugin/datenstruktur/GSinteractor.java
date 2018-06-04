db2681a593b6a200554062ce2ed7a306a17c2fb0
Leen <Leen@Lumberbox.fritz.box>
2018-06-04 18:09:13 +0200

    Redundanz gelöscht

--- GSplugin/src/com/github/herbert/gsplugin/datenstruktur/GSinteractor.java
+++ GSplugin/src/com/github/herbert/gsplugin/datenstruktur/GSinteractor.java
@@ -5,8 +5,6 @@ public interface GSinteractor {
 	
 	//Interface für alle Objekte, die mit einem GS interagieren können
 	//Spieler oder Stadt oder...
-	public boolean isPlayer();
-	public boolean isTown();
 	
 	//UUID für Spieler
 	//Wenn das Objekt eine Stadt ist, dann muss die UUID als TOWN<Stadtname> oder so gesetzt werden.
--- GSplugin/src/com/github/herbert/gsplugin/datenstruktur/TempHerbertPlayer.java
+++ GSplugin/src/com/github/herbert/gsplugin/datenstruktur/TempHerbertPlayer.java
@@ -5,25 +5,11 @@ import org.bukkit.entity.Player;
 public class TempHerbertPlayer implements GSinteractor{
 	Player bukkplayer;
 	Member[] Members=new Member[1];;
-    private Member[] Member;
+        private Member[] Member;
 	public TempHerbertPlayer(Player p) {
 		bukkplayer=p;
         Member[0]=new Member(p,(byte)-1);
-	}
-	
-	public Player getBukkitPlayer() {
-		return bukkplayer;
-	}
-
-	@Override
-	public boolean isPlayer() {
-		return true;
-	}
-
-	@Override
-	public boolean isTown() {
-		return false;
-	}
+        }
 	
 	@Override
 	public Member[] getMembers() {
