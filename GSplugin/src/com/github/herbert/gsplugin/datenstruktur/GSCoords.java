0a078c898b9e1e9cd10bcc99bbab64ada71e20d9
Leen <Leen@Lumberbox.fritz.box>
2018-06-04 21:22:18 +0200

    Chunks zählen jetzt als GS

--- GSplugin/src/com/github/herbert/gsplugin/datenstruktur/GSCoords.java
+++ GSplugin/src/com/github/herbert/gsplugin/datenstruktur/GSCoords.java
@@ -7,7 +7,6 @@ package com.github.herbert.gsplugin.datenstruktur;
 
 import org.bukkit.Location;
 
-import com.github.herbert.gsplugin.GSplugin;
 
 /**
  *
@@ -16,55 +15,17 @@ import com.github.herbert.gsplugin.GSplugin;
 public class GSCoords {
 	
 	//Anm.: Die Koordinate selbst gehört noch zum GS
-    int x1;
-    int z1;
-    int x2;
-    int z2;
-    int lowestY;
+    int x;
+    int z;
     
     //Konstruktor für Location
-    public GSCoords(Location loc1, Location loc2, GSplugin plugin){
-        x1=loc1.getBlockX();
-        z1=loc1.getBlockZ();
-        x2=loc2.getBlockX();
-        z2=loc2.getBlockZ();
-        //1. Koordinate ist immer der kleinere Wert
-        if(x1>x2) {
-        	int i = x1;
-        	x2=x1;
-        	x1=i;
-        }
-        if(z1>z2) {
-        	int i=z1;
-        	z2=z1;
-        	z1=i;
-        }
-    }
-    
-    //Konstruktor für int-Coords
-    public GSCoords(int x, int z, int x2, int z2, GSplugin plugin){
-        this.x1=x;
-        this.z1=z;
-        this.x2=x2;
-        this.z2=z2;
-        //1. Koordinate ist immer der kleinere Wert
-        if(x1>x2) {
-        	int i = x1;
-        	x2=x1;
-        	x1=i;
-        }
-        if(z1>z2) {
-        	int i=z1;
-        	z2=z1;
-        	z1=i;
-        }
-        lowestY = plugin.getConfigInt("gs.lowestYBlock");
+    public GSCoords(int x ,int z){
+        this.x=x;
+        this.z=z;
     }
     
     public boolean equals(GSCoords c){
-    	
-    	
-        if(c.x1==x1&&c.z1==z1){
+        if(c.x==x&&c.z==z){
             return true;
         }
         return false;
@@ -72,8 +33,7 @@ public class GSCoords {
     
     //Prüfe, ob eine Location innerhalb eines GS liegt
     public boolean hasLocation(Location loc) {
-    	if(x1<=loc.getBlockX() && loc.getBlockZ()<=z1 && x2>=loc.getBlockX() && z2>=loc.getBlockZ())
-    		return true;
-    	return false;
+    	if(loc.getChunk().getX()==x&&loc.getChunk().getZ()==z) return true;
+        return false;
     }
 }
