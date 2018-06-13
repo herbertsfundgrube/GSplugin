bf1e00336e83124322f830d4ab74febab447098d
Lumberjack <Lumberjack@134.61.184.184>
2018-06-13 18:00:01 +0200

    FileinteractorXP

---
+++ GSplugin/src/com/github/herbert/playerplugin/XP/FileInterfaceXP.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.herbert.playerplugin.XP;
+import com.github.herbert.playerplugin.*;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ *
+ * @author Leen
+ */
+public class FileInterfaceXP {
+	
+	
+	PlayerPlugin playerplugin;
+	public File gsfile;
+	public File gsinteractorsfile;
+	
+	public FileInterfaceXP(PlayerPlugin plugin) {
+		this.playerplugin = plugin;
+	}
+    public void Laden(){
+        //todo
+    }
+    public void Speichern(){
+        //todo
+    }
+    
+}
